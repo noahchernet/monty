e42ea48a99ed50ac2726c6f925442bec7afe8a03
Noah Chernet <noahchernet@yahoo.com>
2021-12-04 20:33:50 +0300

    Push is now stricter, when number is passed, the number should have spaces around it at least

--- push.c
+++ push.c
@@ -1,6 +1,5 @@
 #include "monty.h"
 
-FILE *file;
 /**
  * push - push @push_num to the top of the stack
  * @stack: the doubly linked list to push @push_num to the top of
@@ -13,11 +12,24 @@ FILE *file;
 void push(stack_t **stack, char *l, int ln, int i)
 {
 	/* String returned from strtol, to check if it has run correctly */
-	char *return_string = "";
+	char *return_string = "", a = (l + i + 4 + 1)[0];
+	int not_num = 0, j;
 
 	long push_num = strtol(l + i + 4, &return_string, 10);
 
-	if (return_string == l + i + 4)
+	for (j = 1; a != 0; j++)
+	{
+		a = (l + i + 4 + j)[0];
+		if ((a >= 48 && a <= 57) || a == ' ' || a == '\n' || a == 0 || a == '-')
+		{
+			if ((l + i + 4 + j + 1)[0] == ' ' || (l + i + 4 + j + 1)[0] == '\n')
+				break;
+			continue;
+		}
+		else
+			not_num = 1;
+	}
+	if (return_string == l + i + 4 || not_num)
 	{
 		free_dlistint(*stack);
 		fprintf(stderr, "L%d: usage: push integer\n", ln);
